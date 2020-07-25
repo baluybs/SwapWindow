a737d738a10a8228ca884be2b6e4af45ed2c135b
YELUBANDI BALA SUBRAHMANYAM <yelubandibalas@gmail.com>
2020-07-25 20:50:25 +0530

    Assignment code

---
+++ ShopCartMap.java
@@ -0,0 +1,107 @@
+package com.baluybs.shop;
+import java.util.*;
+
+class ProductMap{
+	private String itemName;
+	private int itemPrice;
+	private int quantity;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	@Override
+	public String toString() {
+		return itemName + itemPrice + quantity+"\n";
+	}
+}
+public class ShopCartMap {
+	Map<Integer, ProductMap> m=new HashMap<>();
+	private ProductMap p=null;
+	static double totalPrice=0.0;
+	public void showItems() {
+		System.out.println("Pid    Products    Price(rs)");
+		System.out.println("1      kurkure     10");
+		System.out.println("2      Lays        20");
+		System.out.println("3      Oreo        40");
+	}
+	public void addProduct(int pid,int qnty) {
+		if (pid==1) {
+			if (m.containsKey(pid)) {
+				ProductMap k=(ProductMap)m.get(pid);
+				k.setQuantity(qnty+(int)k.getQuantity());
+				m.put(pid, k);
+			}else {
+			p=new ProductMap();
+			p.setItemName("kurkure");
+			p.setItemPrice(10);
+			p.setQuantity(qnty);
+			m.put(1, p);
+			}
+		}
+		else if (pid==2) {
+			if (m.containsKey(pid)) {
+				ProductMap k=(ProductMap)m.get(pid);
+				k.setQuantity(qnty+(int)k.getQuantity());
+				m.put(pid, k);
+			}else {
+			p=new ProductMap();
+			p.setItemName("Lays");
+			p.setItemPrice(20);
+			p.setQuantity(qnty);
+			m.put(2, p);
+			}
+		}
+		else if (pid==3) {
+			if (m.containsKey(pid)) {
+				ProductMap k=(ProductMap)m.get(pid);
+				k.setQuantity(qnty+(int)k.getQuantity());
+				m.put(pid, k);
+			}else {
+			p=new ProductMap();
+			p.setItemName("Oreo");
+			p.setItemPrice(40);
+			p.setQuantity(qnty);
+			m.put(3, p);
+			}
+		}else {
+			System.err.println("Please enter ProductId in the given list");
+		}
+	}
+	public void removeItem(int pid) {
+		m.remove(pid);
+	}
+	public void displayCart() {
+		System.out.println("Name\tPrice\tqnty");
+		Set key=m.keySet();
+		Iterator itr=key.iterator();
+		System.out.println("..................");
+		while(itr.hasNext()) {
+			Object k= itr.next();
+			System.out.print(m.get(k).getItemName()+"\t"+m.get(k).getItemPrice()+"\t"+m.get(k).getQuantity()+"\n");
+			totalPrice+=(int)m.get(k).getItemPrice()*(int)m.get(k).getQuantity();
+			System.out.println("..................");
+		}
+		System.out.println();
+		System.out.println(totalPrice);
+	}
+//	public static void main(String[] args) {
+//		ShopCartMap c=new ShopCartMap();
+//		c.addProduct(1, 2);
+//		c.addProduct(2, 2);
+//		c.displayCart();
+//	}
+}
---
+++ Test.java
@@ -0,0 +1,40 @@
+package com.baluybs.shop;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		while(true) {
+			System.out.println("Press 1 for List of Products");
+			System.out.println("Press 2 to add Products");
+			System.out.println("Press 3 to remove Products");
+			System.out.println("Press 4 to view Cart");
+			System.out.println("enter option");
+			int opt=sc.nextInt();
+			select(opt);
+		}
+	}
+	public static void select(int opt) {
+		ShopCartMap c=new ShopCartMap();
+		Scanner scn=new Scanner(System.in);
+		switch(opt)
+		{
+		case 1: c.showItems();
+				break;
+		case 2: System.out.println("Enter Product Id and quantity");
+				int pid=scn.nextInt();
+				int qty=scn.nextInt();
+				c.addProduct(pid, qty);
+				break;
+		case 3: System.out.println("Enter Product Id to remove product");
+				int id=scn.nextInt();
+				c.removeItem(id);
+				break;
+		case 4: c.displayCart();
+				break;
+		default :System.err.println("Please enter a valid input");
+		}
+	}
+}
